7e4cef4ea2f97ef3ef050913b2c38df09f6a8996
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-09-30 10:39:21 +0300

    Initial commit

---
+++ lib/numerify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "numerify/version"
+
+module Numerify
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/numerify/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Numerify
+  VERSION = "0.1.0"
+end
