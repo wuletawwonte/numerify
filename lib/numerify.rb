04872c9b0de16002bb396de08be436c6a7237d6f
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-10-04 11:31:35 +0300

    Fix require relative error

--- lib/numerify.rb
+++ lib/numerify.rb
@@ -3,5 +3,3 @@
 require_relative "numerify/version"
 require_relative "numerify/core_ext/string"
 require_relative "numerify/core_ext/integer"
-
-puts 2004.convert
--- lib/numerify/core_ext/integer.rb
+++ lib/numerify/core_ext/integer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "numerify/utils/shared"
+require_relative "../utils/shared"
 
 # A patch to add the convert method to the Integer class.
 class Integer
